71dbe2f2ffcfee4c59a3c351a8e9d20c56dea4c9
Alonso-Zr <alonsoseguro27@gmail.com>
2026-07-12 02:44:05 -0500

    fix: añadir metodo calcularConsumo() para ProovedorInternet

--- src/ProovedorInternet.java
+++ src/ProovedorInternet.java
@@ -1,3 +1,15 @@
 public abstract class ProovedorInternet {
-    public abstract PlanDatos crearPlanDatos(String tipo);
+
+    protected abstract PlanDatos crearPlanDatos(String tipo);
+
+    public void calcularConsumo(String tipo, int numeroMegasConsumido) {
+        PlanDatos plan = crearPlanDatos(tipo);
+
+        if (plan != null) {
+            float pago = plan.calculoPago(numeroMegasConsumido);
+            System.out.println(plan.toString() + " Pago: " + pago + " por " + numeroMegasConsumido + " megas");
+        } else {
+            System.out.println("Plan no válido.");
+        }
+    }
 }
